9419a17d23147922ce0d2df9c5cc459d09053f48
Fatih Durmaz <fatih.durmaz@outlook.com>
2023-12-25 21:50:59 +0300

    Initial Commit

---
+++ TipKitExample/TipKitExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipKitExampleApp.swift
+//  TipKitExample
+//
+//  Created by Fatih Durmaz on 25.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct TipKitExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
